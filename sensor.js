0f791ca0fbd0076b304814810cf49d5f85892116
yuno18120 <yuno18120@icloud.com>
2023-06-28 13:58:54 +0900

    third commit

--- sensor.js
+++ sensor.js
@@ -1,27 +1,39 @@
-window.addEventListener("devicemotion", devicemotion);
-window.addEventListener("deviceorientation", deveceorientation,false);
+const SCREEN_WIDTH = 480;		// キャンバス幅
+const SCREEN_HEIGHT = 480;	// キャンバス高さ
 
-function devicemotion(e) {
-    let x = e.accelerationIncludingGravity.x;
-    let y = e.accelerationIncludingGravity.y;
-    let z = e.accelerationIncludingGravity.z;
+/*
+ * グローバル変数
+ */
+var canvas = null;		// キャンバス
+var g = null;				// コンテキスト
 
-    let result1 = document.getElementById("t0");
-    result1.innerHTML = "重力加速度<br />" +
-        "X:" + x.toFixed(2) + "(m/s^2)<br />" +
-        "Y:" + y.toFixed(2) + "(m/s^2)<br />" +
-        "Z:" + z.toFixed(2) + "(m/s^2)<br />";
-}
+/*
+ * 起動処理
+ */
+window.onload = function(){
+	// キャンバス情報取得
+	canvas = document.getElementById("canvas");
+	g = canvas.getContext("2d");
 
-function deveceorientation() {
-    var alpha = e.alpha;
-    var beta = e.beta;
-    var gamma = e.gamma;
+	// キャンバスサイズ設定
+	canvas.width = SCREEN_WIDTH;
+	canvas.height = SCREEN_HEIGHT;
+	
+	// 加速度センサーイベント処理
+	window.addEventListener("deviceorientation", function(e){
+		// キャンバスをクリア
+		g.fillStyle = "#ddd";
+		g.fillRect(0, 0, canvas.width, canvas.height);
 
-    var result2 = document.getElementById("t1");
-    result2.innerHTML = "ジャイロセンサー<br />" +
-        "alpha:" + alpha.toFixed(2) + "°<br />" +
-        "beta :" + beta.toFixed(2) + "°<br />" +
-        "gamma:" + gamma.toFixed(2) + "°<br />";
-}
+		// キャンバスに加速度センサーの各プロパティを表示
+		g.font = "24px system";
+		g.fillStyle = "#555";
+		g.textBaseline = "top";	// 文字のベースラインを上に
+		
+		g.fillText("alpha: " + e.alpha, 20, 20);			// z軸
+		g.fillText("beta: " + e.beta, 20, 60);			// x軸
+		g.fillText("gamma: " + e.gamma, 20, 100);	// y軸
 
+		g.fillText("absolute: " + e.absolute, 20, 180);	// 相対値: true 絶対値: false
+	}, false);
+};
