6030a9ca9ab1f404718da6b3082d5e18d137b456
yuno18120 <yuno18120@icloud.com>
2023-06-28 13:13:54 +0900

    third commit

--- sensor.js
+++ sensor.js
@@ -1,20 +1,29 @@
 //センサー
 
-function ClickRequestDeviceSensor(){
-    DeviceOrientationEvent.requestPermission().then(function(responce){
-        if(responce == 'granted'){
-            window.addEventListener("deviceorientation",deviceOrientation);
-        }
+window.addEventListener("devicemotion", devicemotion);
+window.addEventListener("deviceorientation", deveceorientation,false);
 
-    })
+function devicemotion(e) {
+    let x = e.accelerationIncludingGravity.x;
+    let y = e.accelerationIncludingGravity.y;
+    let z = e.accelerationIncludingGravity.z;
+
+    let result1 = document.getElementById("t0");
+    result1.innerHTML = "重力加速度<br />" +
+        "X:" + x.toFixed(2) + "(m/s^2)<br />" +
+        "Y:" + y.toFixed(2) + "(m/s^2)<br />" +
+        "Z:" + z.toFixed(2) + "(m/s^2)<br />";
 }
 
-function deviceOrientation(e){
-    let bata = e.bata;
-    let alpha = e.alpha;
-    let gamma = e.gamma;
+function deveceorientation() {
+    var alpha = e.alpha;
+    var beta = e.beta;
+    var gamma = e.gamma;
 
-    document.getElementById("t0").innerText=bata;
-    document.getElementById("t1").innerText=alpha;
-    document.getElementById("t2").innerText=gamma;
+    var result2 = document.getElementById("t1");
+    result2.innerHTML = "ジャイロセンサー<br />" +
+        "alpha:" + alpha.toFixed(2) + "°<br />" +
+        "beta :" + beta.toFixed(2) + "°<br />" +
+        "gamma:" + gamma.toFixed(2) + "°<br />";
 }
+
